4631b6abc3d059f8ba1802f2c501b3fbc23aec89
yuanx749 <yuanx749@gmail.com>
2020-04-21 19:06:38 +0200

    Add mini-batch, predict probability

--- nn.py
+++ nn.py
@@ -3,6 +3,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.utils.data import TensorDataset, DataLoader
 
 def timer(func):
     def wrapper(*args, **kwargs):
@@ -27,12 +28,17 @@ class MLP(nn.Module):
         return y_pred
 
 class MLPClassifier:
-    def __init__(self, hidden_layer_sizes=(100,), alpha=0.0001, learning_rate=0.001, max_iter=200, random_state=None, verbose=False):
+    def __init__(
+        self, hidden_layer_sizes=(100,), alpha=0.0001, 
+        batch_size=200, learning_rate=0.001, max_iter=200, 
+        shuffle=True, random_state=None, verbose=False):
         self.model = None
         self.hidden_layer_sizes = hidden_layer_sizes
         self.alpha = alpha
+        self.batch_size = batch_size
         self.learning_rate = learning_rate
         self.max_iter = max_iter
+        self.shuffle = shuffle
         self.random_state = random_state
         self.verbose = verbose
         self.loss_curve = []
@@ -50,20 +56,27 @@ class MLPClassifier:
         # n_classes = int(np.max(y)) + 1
         X = torch.from_numpy(X).float()
         y = torch.from_numpy(y).long()
+        dataset = TensorDataset(X, y)
+        self.batch_size = min(self.batch_size, n_samples)
+        dataloader = DataLoader(dataset, batch_size=self.batch_size, shuffle=self.shuffle)
         self.model = MLP(n_features, self.hidden_layer_sizes, n_classes)
         loss_fn = nn.CrossEntropyLoss()
         optimizer = torch.optim.Adam(self.model.parameters(), lr=self.learning_rate, weight_decay=self.alpha)
 
         self.model.train()
         for epoch in range(self.max_iter):
-            y_pred = self.model(X)
-            loss = loss_fn(y_pred, y)
-            self.loss_curve.append(loss.item())
+            accumulated_loss = 0.0
+            for X, y in dataloader:
+                y_pred = self.model(X)
+                loss = loss_fn(y_pred, y)
+                accumulated_loss += loss.item() * y.shape[0]
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+            loss_ = accumulated_loss / n_samples
+            self.loss_curve.append(loss_)
             if self.verbose and epoch % 100 == 0:
-                print(epoch, loss.item())
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+                print(epoch, loss_)
         return self
 
     def predict(self, X):
@@ -72,6 +85,13 @@ class MLPClassifier:
         output = self.model(X)
         y_pred = torch.argmax(output, dim=1).numpy()
         return y_pred
+
+    def predict_proba(self, X):
+        X = torch.from_numpy(X).float()
+        self.model.eval()
+        output = self.model(X)
+        y_prob = F.softmax(output, dim=1).detach().numpy()
+        return y_prob
 
     def score(self, X, y):
         return np.sum(self.predict(X) == y) / y.size
