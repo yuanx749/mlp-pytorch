cbf3ab821bd8c02e0e06e11d512bf82bbbe5a4ed
yuanx749 <yuanx749@gmail.com>
2020-06-16 14:48:16 +0200

    Add CNN, RNN

--- nn.py
+++ nn.py
@@ -1,4 +1,6 @@
 import time
+from abc import ABCMeta, abstractmethod
+
 import numpy as np
 import torch
 import torch.nn as nn
@@ -19,17 +21,54 @@ class MLP(nn.Module):
     def forward(self, x):
         for linear in self.linear[:-1]:
             x = F.relu(linear(x))
-        y_pred = self.linear[-1](x)
-        return y_pred
+        x = self.linear[-1](x)
+        return x
 
-class MLPClassifier(BaseEstimator, ClassifierMixin):
+class CNN(nn.Module):
+    def __init__(self, D_in, D_out):
+        super().__init__()
+        f = 5
+        self.conv1 = nn.Conv1d(1, 6, f)
+        self.pool = nn.MaxPool1d(2)
+        l_out = self._output_size(D_in, f, 1, 0)
+        l_out = self._output_size(l_out, 2, 2, 0)
+        self.conv2 = nn.Conv1d(6, 16, f)
+        l_out = self._output_size(l_out, f, 1, 0)
+        l_out = self._output_size(l_out, 2, 2, 0)
+        self.l_out = l_out
+        self.fc1 = nn.Linear(16 * l_out, 128)
+        self.fc2 = nn.Linear(128, 64)
+        self.fc3 = nn.Linear(64, D_out)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * self.l_out)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+    
+    def _output_size(self, l_in, kernel_size, stride, padding):
+        return int(np.floor((l_in+2*padding-kernel_size)/stride + 1))
+
+class RNN(nn.Module):
+    def __init__(self, D_in, D_out):
+        super().__init__()
+        self.rnn = nn.GRU(input_size=1, hidden_size=32, num_layers=2, batch_first=True)
+        self.fc = nn.Linear(32, D_out)
+
+    def forward(self, x):
+        x, _ = self.rnn(x)
+        x = self.fc(x[:, -1, :])
+        return x
+
+class BaseClassifier(BaseEstimator, ClassifierMixin, metaclass=ABCMeta):
+    @abstractmethod
     def __init__(
-        self, hidden_layer_sizes=(100,), alpha=0.0001, 
-        batch_size=200, learning_rate=0.001, max_iter=200, 
-        shuffle=True, random_state=None, verbose=False, 
-        validation_fraction=0.1):
-        self.model = None
-        self.hidden_layer_sizes = hidden_layer_sizes
+        self, alpha, batch_size, learning_rate, 
+        max_iter, shuffle, random_state, verbose, 
+        validation_fraction):
         self.alpha = alpha
         self.batch_size = batch_size
         self.learning_rate = learning_rate
@@ -38,9 +77,6 @@ class MLPClassifier(BaseEstimator, ClassifierMixin):
         self.random_state = random_state
         self.verbose = verbose
         self.validation_fraction = validation_fraction
-        self.loss_curve_ = []
-        self.training_scores_ = []
-        self.validation_scores_ = []
 
     def timer(func):
         def wrapper(self, *args, **kwargs):
@@ -50,14 +86,12 @@ class MLPClassifier(BaseEstimator, ClassifierMixin):
                 print('{} seconds'.format(time.time() - start))
             return value
         return wrapper
-    
-    @timer
-    def fit(self, X, y):
-        '''
-        X: ndarray of shape (n_samples, n_features)
-        y: ndarray of shape (n_samples,)
-        '''
-        X, X_val, y, y_val = train_test_split(
+
+    def _numpy_to_tensor_x(self, X):
+        return torch.from_numpy(X).float()
+
+    def _fetch_dataloader(self, X, y):
+        X, self.X_val, y, self.y_val = train_test_split(
             X, y, 
             test_size=self.validation_fraction, 
             random_state=self.random_state, 
@@ -66,18 +100,24 @@ class MLPClassifier(BaseEstimator, ClassifierMixin):
         self.X_ = X
         self.y_ = y
         self.classes_, y = np.unique(y, return_inverse=True)
-        
+        self.n_samples, self.n_features = X.shape
+        self.n_classes = np.unique(y).size
+
         if isinstance(self.random_state, int):
             torch.manual_seed(self.random_state)
-        n_samples, n_features = X.shape
-        n_classes = np.unique(y).size
-        X = torch.from_numpy(X).float()
+        X = self._numpy_to_tensor_x(X)
         y = torch.from_numpy(y).long()
         dataset = TensorDataset(X, y)
-        self.batch_size = min(self.batch_size, n_samples)
+        self.batch_size = min(self.batch_size, self.n_samples)
         dataloader = DataLoader(dataset, batch_size=self.batch_size, shuffle=self.shuffle)
+        return dataloader
 
-        self.model = MLP(n_features, self.hidden_layer_sizes, n_classes)
+    @timer
+    def _train(self, dataloader):
+        self.loss_curve_ = []
+        self.training_scores_ = []
+        self.validation_scores_ = []
+        
         loss_fn = nn.CrossEntropyLoss() # combines nn.LogSoftmax() and nn.NLLLoss()
         optimizer = torch.optim.Adam(
             self.model.parameters(), 
@@ -96,10 +136,10 @@ class MLPClassifier(BaseEstimator, ClassifierMixin):
                 optimizer.zero_grad()
                 loss.backward()
                 optimizer.step()
-            loss_ = accumulated_loss / n_samples
+            loss_ = accumulated_loss / self.n_samples
             self.loss_curve_.append(loss_)
             self.training_scores_.append(self.score(self.X_, self.y_))
-            self.validation_scores_.append(self.score(X_val, y_val))
+            self.validation_scores_.append(self.score(self.X_val, self.y_val))
             if self.verbose and epoch % 10 == 0:
                 print('{:8d}{:8.4f}{:18.4f}{:18.4f}'.format(
                     epoch + 1, loss_, self.training_scores_[-1], self.validation_scores_[-1]))
@@ -108,7 +148,7 @@ class MLPClassifier(BaseEstimator, ClassifierMixin):
     def predict(self, X):
         check_is_fitted(self, ['X_', 'y_'])
         X = check_array(X)
-        X = torch.from_numpy(X).float()
+        X = self._numpy_to_tensor_x(X)
         self.model.eval()
         output = self.model(X)
         y_pred = self.classes_[torch.argmax(output, dim=1).numpy()]
@@ -117,8 +157,80 @@ class MLPClassifier(BaseEstimator, ClassifierMixin):
     def predict_proba(self, X):
         check_is_fitted(self, ['X_', 'y_'])
         X = check_array(X)
-        X = torch.from_numpy(X).float()
+        X = self._numpy_to_tensor_x(X)
         self.model.eval()
         output = self.model(X)
         y_prob = F.softmax(output, dim=1).detach().numpy()
         return y_prob
+
+class MLPClassifier(BaseClassifier):
+    def __init__(
+        self, hidden_layer_sizes=(100, 100), alpha=0.0001, 
+        batch_size=200, learning_rate=0.001, max_iter=200, 
+        shuffle=True, random_state=None, verbose=False, 
+        validation_fraction=0.2):
+        # no *args or **kwargs
+        super().__init__(
+            alpha=alpha, batch_size=batch_size, learning_rate=learning_rate, 
+            max_iter=max_iter, shuffle=shuffle, random_state=random_state, verbose=verbose, 
+            validation_fraction=validation_fraction)
+        self.hidden_layer_sizes = hidden_layer_sizes
+
+    def fit(self, X, y):
+        '''
+        X: ndarray of shape (n_samples, n_features)
+        y: ndarray of shape (n_samples,)
+        '''
+        dataloader = self._fetch_dataloader(X, y)
+        self.model = MLP(self.n_features, self.hidden_layer_sizes, self.n_classes)
+        return self._train(dataloader)
+
+class CNNClassifier(BaseClassifier):
+    def __init__(
+        self, alpha=0.0001, 
+        batch_size=200, learning_rate=0.001, max_iter=200, 
+        shuffle=True, random_state=None, verbose=False, 
+        validation_fraction=0.2):
+        super().__init__(
+            alpha=alpha, batch_size=batch_size, learning_rate=learning_rate, 
+            max_iter=max_iter, shuffle=shuffle, random_state=random_state, verbose=verbose, 
+            validation_fraction=validation_fraction)
+
+    def _numpy_to_tensor_x(self, X):
+        return torch.from_numpy(X).float().unsqueeze(1) # input shape: (N, C_in, L_in)
+
+    def fit(self, X, y):
+        '''
+        X: ndarray of shape (n_samples, n_features)
+        y: ndarray of shape (n_samples,)
+        '''
+        dataloader = self._fetch_dataloader(X, y)
+        self.model = CNN(self.n_features, self.n_classes)
+        return self._train(dataloader)
+
+class RNNClassifier(BaseClassifier):
+    def __init__(
+        self, alpha=0.0001, 
+        batch_size=200, learning_rate=0.001, max_iter=200, 
+        shuffle=True, random_state=None, verbose=False, 
+        validation_fraction=0.2):
+        super().__init__(
+            alpha=alpha, batch_size=batch_size, learning_rate=learning_rate, 
+            max_iter=max_iter, shuffle=shuffle, random_state=random_state, verbose=verbose, 
+            validation_fraction=validation_fraction)
+
+    def _numpy_to_tensor_x(self, X):
+        return torch.from_numpy(X).float().unsqueeze(2) # input shape: (batch, seq_len, input_size)
+
+    def fit(self, X, y):
+        '''
+        X: ndarray of shape (n_samples, n_features)
+        y: ndarray of shape (n_samples,)
+        '''
+        dataloader = self._fetch_dataloader(X, y)
+        self.model = RNN(self.n_features, self.n_classes)
+        return self._train(dataloader)
+
+if __name__ == "__main__":
+    from sklearn.utils.estimator_checks import check_estimator
+    check_estimator(MLPClassifier())
--- run_experiment.py
+++ run_experiment.py
@@ -10,32 +10,33 @@ import data_loader
 import nn
 import plotting
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Run an experiment')
-    parser.add_argument('--output', type=str, default='output', help='Output directory')
-    parser.add_argument('--dataset', type=str, default='biodeg.csv', help='Input dataset file')
-    parser.add_argument('--params', type=str, default='params.yml', help='Hyperparameters YAML')
-    parser.add_argument('--plot', action='store_false', help='NOT plot learning curves')
-    parser.add_argument('--verbose', action='store_false', help='NOT print progress messages to stdout')
-    args = parser.parse_args()
+parser = argparse.ArgumentParser(description='Run an experiment')
+parser.add_argument('--output', default='output', type=str, help='Output directory')
+parser.add_argument('--dataset', default='biodeg.csv', type=str, help='Input dataset file')
+parser.add_argument('--model', default='MLP', choices=['MLP', 'CNN', 'RNN'], help='Model type')
+parser.add_argument('--params', default='params1.yml', type=str, help='Hyperparameters YAML')
+parser.add_argument('--plot', action='store_false', help='NOT plot learning curves')
+parser.add_argument('--verbose', action='store_false', help='NOT print progress messages to stdout')
+args = parser.parse_args()
 
-    log_dir = os.path.abspath('log')
-    os.makedirs(log_dir, exist_ok=True)
-    log_file_path = os.path.join(log_dir, time.strftime('%Y%m%d-%H%M%S.log'))
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.DEBUG)
-    console_handler = logging.StreamHandler(stream=sys.stdout)
-    console_handler.setLevel(logging.INFO)
-    console_handler.setFormatter(formatter)
-    logger.addHandler(console_handler)
-    file_handler = logging.FileHandler(log_file_path)
-    file_handler.setLevel(logging.INFO)
-    file_handler.setFormatter(formatter)
-    logger.addHandler(file_handler)
-    if not args.verbose:
-        console_handler.setLevel(logging.WARNING)
+log_dir = os.path.abspath('log')
+os.makedirs(log_dir, exist_ok=True)
+log_file_path = os.path.join(log_dir, time.strftime('%Y%m%d-%H%M%S.log'))
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+console_handler = logging.StreamHandler(stream=sys.stdout)
+console_handler.setLevel(logging.INFO)
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
+file_handler = logging.FileHandler(log_file_path)
+file_handler.setLevel(logging.INFO)
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+if not args.verbose:
+    console_handler.setLevel(logging.WARNING)
 
+def main():
     input_path = os.path.abspath(os.path.join('./data', args.dataset))
     dataset = os.path.splitext(args.dataset)[0]
     logger.info('Load {}'.format(input_path))
@@ -43,19 +44,20 @@ if __name__ == "__main__":
     X_train, X_test, y_train, y_test = data_loader.load(input_path, **params)
     logger.info('Split into train and test subsets: {}'.format(params))
     
-    params_path = os.path.abspath(args.params)
+    params_path = os.path.abspath(os.path.join('./params', args.params))
     with open(params_path) as file_:
         params = yaml.load(file_, Loader=yaml.SafeLoader)
     logger.info('Load {}'.format(params_path))
     logger.info('Hyperparameters: {}'.format(params))
-    mlp = nn.MLPClassifier(**params)
-    estimator = mlp.__class__.__name__
+    models = {'MLP': nn.MLPClassifier, 'CNN': nn.CNNClassifier, 'RNN': nn.RNNClassifier}
+    clf = models[args.model](**params)
+    estimator = clf.__class__.__name__
     logger.info('Train {} on {}'.format(estimator, dataset))
-    mlp.fit(X_train, y_train)
+    clf.fit(X_train, y_train)
     
     output_dir = os.path.abspath(args.output)
     os.makedirs(output_dir, exist_ok=True)
-    csv_log = pd.DataFrame({'loss': mlp.loss_curve_, 'train_score': mlp.training_scores_, 'val_score': mlp.validation_scores_})
+    csv_log = pd.DataFrame({'loss': clf.loss_curve_, 'train_score': clf.training_scores_, 'val_score': clf.validation_scores_})
     csv_log_path = os.path.join(output_dir, time.strftime('%Y%m%d-%H%M%S.csv'))
     csv_log.to_csv(csv_log_path)
     logger.info('Save learning log to {}'.format(csv_log_path))
@@ -65,6 +67,13 @@ if __name__ == "__main__":
         plotting.plot_learning_curve(csv_log_path, '{} on {}'.format(estimator, dataset), plot_path)
         logger.info('Save learning curves to {}'.format(plot_path))
     
-    logger.info('Training score: {}'.format(mlp.score(X_train, y_train)))
-    logger.info('Testing score: {}'.format(mlp.score(X_test, y_test)))
+    logger.info('Training score: {}'.format(clf.score(X_train, y_train)))
+    logger.info('Testing score: {}'.format(clf.score(X_test, y_test)))
     logger.info('Done')
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as err:
+        logger.error(err, exc_info=True)
+        raise
